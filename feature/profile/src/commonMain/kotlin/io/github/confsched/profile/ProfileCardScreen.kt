6a59a078edfe98bb1afb63a7b8e40d9280e9a3c3
todayama_r <13657682+Corvus400@users.noreply.github.com>
2025-09-03 16:16:00 +0900

    :recycle: The background image now displays on the ProfileCardScreen according to the selected theme.

--- feature/profile/src/commonMain/kotlin/io/github/confsched/profile/ProfileCardScreen.kt
+++ feature/profile/src/commonMain/kotlin/io/github/confsched/profile/ProfileCardScreen.kt
@@ -1,6 +1,12 @@
 package io.github.confsched.profile
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -17,13 +23,16 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -32,8 +41,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import io.github.confsched.profile.components.CardPreviewImageBitmaps
 import io.github.confsched.profile.components.FlippableProfileCard
@@ -42,11 +53,15 @@ import io.github.droidkaigi.confsched.droidkaigiui.KaigiPreviewContainer
 import io.github.droidkaigi.confsched.droidkaigiui.component.AnimatedTextTopAppBar
 import io.github.droidkaigi.confsched.droidkaigiui.compositionlocal.safeDrawingWithBottomNavBar
 import io.github.droidkaigi.confsched.model.profile.Profile
+import io.github.droidkaigi.confsched.model.profile.ProfileCardTheme
 import io.github.droidkaigi.confsched.profile.ProfileRes
 import io.github.droidkaigi.confsched.profile.edit
 import io.github.droidkaigi.confsched.profile.profile_card_title
 import io.github.droidkaigi.confsched.profile.share
+import io.github.droidkaigi.confsched.profile.shareable_card_background_day
+import io.github.droidkaigi.confsched.profile.shareable_card_background_night
 import kotlinx.coroutines.launch
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -61,6 +76,33 @@ fun ProfileCardScreen(
     var shareableProfileCardRenderResult: ImageBitmap? by remember { mutableStateOf(null) }
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    val backgroundAlpha = remember { Animatable(0f) }
+    val backgroundRes = when (uiState.profile.theme) {
+        ProfileCardTheme.DarkPill,
+        ProfileCardTheme.DarkDiamond,
+        ProfileCardTheme.DarkFlower,
+        -> ProfileRes.drawable.shareable_card_background_day
+        ProfileCardTheme.LightPill,
+        ProfileCardTheme.LightDiamond,
+        ProfileCardTheme.LightFlower,
+        -> ProfileRes.drawable.shareable_card_background_night
+    }
+
+    LaunchedEffect(shareableProfileCardRenderResult) {
+        if (shareableProfileCardRenderResult != null) {
+            backgroundAlpha.snapTo(0f)
+            backgroundAlpha.animateTo(
+                targetValue = 1f,
+                animationSpec = tween(
+                    durationMillis = 500,
+                    easing = FastOutSlowInEasing,
+                ),
+            )
+        } else {
+            backgroundAlpha.snapTo(0f)
+        }
+    }
+
     // Not displayed, just for generating shareable card image
     ShareableProfileCard(
         theme = uiState.profile.theme,
@@ -71,65 +113,93 @@ fun ProfileCardScreen(
         onRenderResultUpdate = { shareableProfileCardRenderResult = it },
     )
 
-    Scaffold(
-        topBar = {
-            AnimatedTextTopAppBar(
-                title = stringResource(ProfileRes.string.profile_card_title),
-                scrollBehavior = scrollBehavior,
-            )
-        },
-        contentWindowInsets = WindowInsets.safeDrawingWithBottomNavBar,
-        modifier = modifier,
-    ) { contentPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .nestedScroll(scrollBehavior.nestedScrollConnection)
-                .verticalScroll(rememberScrollState())
-                .padding(horizontal = 16.dp)
-                .padding(contentPadding),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-        ) {
-            FlippableProfileCard(
-                uiState = uiState,
-                modifier = Modifier.alpha(if (shareableProfileCardRenderResult != null) 1f else 0f),
-            )
-            Spacer(Modifier.height(32.dp))
-            Button(
-                enabled = shareableProfileCardRenderResult != null,
-                onClick = {
-                    shareableProfileCardRenderResult?.let {
-                        coroutineScope.launch {
-                            onShareClick(it)
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+    ) {
+        Image(
+            painter = painterResource(backgroundRes),
+            contentDescription = null,
+            modifier = Modifier.matchParentSize(),
+            contentScale = ContentScale.Crop,
+            alignment = Alignment.Center,
+            alpha = backgroundAlpha.value,
+        )
+        Scaffold(
+            topBar = {
+                AnimatedTextTopAppBar(
+                    title = stringResource(ProfileRes.string.profile_card_title),
+                    scrollBehavior = scrollBehavior,
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color.Transparent,
+                    ),
+                    textColor = MaterialTheme.colorScheme.surface,
+                    scrolledTextColor = MaterialTheme.colorScheme.onSurface,
+                )
+            },
+            contentWindowInsets = WindowInsets.safeDrawingWithBottomNavBar,
+            containerColor = Color.Transparent,
+            modifier = modifier.fillMaxSize(),
+        ) { contentPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .nestedScroll(scrollBehavior.nestedScrollConnection)
+                    .verticalScroll(rememberScrollState())
+                    .padding(horizontal = 16.dp)
+                    .padding(contentPadding),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                FlippableProfileCard(
+                    uiState = uiState,
+                    modifier = Modifier.alpha(if (shareableProfileCardRenderResult != null) 1f else 0f),
+                )
+                Spacer(Modifier.height(32.dp))
+                Button(
+                    enabled = shareableProfileCardRenderResult != null,
+                    onClick = {
+                        shareableProfileCardRenderResult?.let {
+                            coroutineScope.launch { onShareClick(it) }
                         }
-                    }
-                },
-                modifier = Modifier.fillMaxWidth(),
-                contentPadding = PaddingValues(18.dp),
-            ) {
-                Row(
-                    horizontalArrangement = Arrangement.End,
-                    modifier = Modifier.weight(1f),
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    contentPadding = PaddingValues(18.dp),
+                    colors = ButtonDefaults.buttonColors().copy(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainerLow,
+                    ),
                 ) {
-                    Icon(
-                        imageVector = Icons.Default.Share,
-                        contentDescription = "Share profile card",
-                        modifier = Modifier.size(18.dp),
+                    Row(
+                        horizontalArrangement = Arrangement.End,
+                        modifier = Modifier.weight(1f),
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Share,
+                            contentDescription = "Share profile card",
+                            modifier = Modifier.size(18.dp),
+                            tint = MaterialTheme.colorScheme.primary,
+                        )
+                        Spacer(Modifier.width(8.dp))
+                    }
+                    Text(
+                        text = stringResource(ProfileRes.string.share),
+                        color = MaterialTheme.colorScheme.primary,
                     )
-                    Spacer(Modifier.width(8.dp))
+                    Spacer(Modifier.weight(1f))
                 }
-                Text(stringResource(ProfileRes.string.share))
-                Spacer(Modifier.weight(1f))
-            }
-            Spacer(Modifier.height(8.dp))
-            OutlinedButton(
-                onClick = onEditClick,
-                modifier = Modifier.fillMaxWidth(),
-                border = null,
-                contentPadding = PaddingValues(18.dp),
-            ) {
-                Text(stringResource(ProfileRes.string.edit))
+                Spacer(Modifier.height(8.dp))
+                OutlinedButton(
+                    onClick = onEditClick,
+                    modifier = Modifier.fillMaxWidth(),
+                    border = null,
+                    contentPadding = PaddingValues(18.dp),
+                ) {
+                    Text(
+                        text = stringResource(ProfileRes.string.edit),
+                        color = MaterialTheme.colorScheme.surface,
+                    )
+                }
             }
         }
     }
